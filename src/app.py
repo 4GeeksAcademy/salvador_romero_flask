31cf46f0753d5a6a2107c9434fa76abfaf7fa973
Salvador Romero <102228468+SalviKC@users.noreply.github.com>
2025-06-11 18:47:54 +0000

    Prueba

--- src/app.py
+++ src/app.py
@@ -1,4 +1,29 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, request, render_template
+import joblib
+import numpy as np
 
-# your code here
+app = Flask(__name__)
+
+model = joblib.load('../models/modelo_personalidad.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predecir', methods=['POST'])
+def predecir():
+    datos = [
+        float(request.form['Time_spent_Alone']),
+        float(request.form['Stage_fear']),
+        float(request.form['Social_event_attendance']),
+        float(request.form['Going_outside']),
+        float(request.form['Drained_after_socializing']),
+        float(request.form['Friends_circle_size']),
+        float(request.form['Post_frequency'])
+    ]
+    prediccion = model.predict([datos])[0]
+
+    return render_template('index.html', prediccion=f'Tu personalidad es: {prediccion}')
+
+if __name__ == '__main__':
+    app.run(debug=True)
